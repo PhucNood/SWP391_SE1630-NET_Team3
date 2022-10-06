bbe12721867f6cc86cdb8bf35637672599b98808
vhnam22 <vhnam2209@gmail.com>
2022-10-06 15:28:03 +0700

    add image, prodcutdetail

---
+++ SWP391_SE1630-NET_Team3/src/java/controller/ProductDetailController.java
@@ -0,0 +1,112 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package controller;
+
+import dao.CategoryDAO;
+import dao.ProductDAO;
+import dao.impl.CategoryDAOImpl;
+import dao.impl.ProductDAOImpl;
+import entity.Category;
+import entity.Image;
+import entity.Product;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.List;
+
+/**
+ *
+ * @author admin
+ */
+public class ProductDetailController extends HttpServlet {
+   
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ProductDetailController</title>");  
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet ProductDetailController at " + request.getContextPath () + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    } 
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        String productID = request.getParameter("productID");
+        ProductDAO ProductDAO = new ProductDAOImpl();
+        //get product by productID
+        Product product = ProductDAO.getProductById(productID);
+        //get Image List of product
+        List<Image> listImg = product.getList();
+        
+        CategoryDAO CategoryDAO = new CategoryDAOImpl();
+        //get category of product
+        Category category = CategoryDAO.getCategoryById(product.getCategoryID());
+        //get product list of category
+        List<Product> listProductInCategory = ProductDAO.getProductListByCategoryID(product.getCategoryID());
+        //remove product in product list of category
+        for(Product item : listProductInCategory){
+            if(item.getProductID() == product.getProductID()){
+                listProductInCategory.remove(item);
+                break;
+            }
+        }
+        request.setAttribute("listProduct", listProductInCategory);
+        request.setAttribute("category", category);
+        request.setAttribute("listImg", listImg);
+        request.setAttribute("product", product);
+        request.getRequestDispatcher("view/productDetail.jsp").forward(request, response);
+    } 
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
